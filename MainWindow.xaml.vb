3348e5d293b13e3549abaa470a44737f84cc6722
Bells Branding <erinbelldeveloper@gmail.com>
2018-10-02 11:15:31 -0700

    Update MainWindow.xaml.vb

--- MainWindow.xaml.vb
+++ MainWindow.xaml.vb
@@ -1,19 +1,18 @@
-﻿Imports MahApps.Metro.Controls
+Imports MahApps.Metro.Controls
 Imports System.ComponentModel
 Imports System.IO
 Imports System.Text
-Imports System.Collections.ObjectModel
-Imports MahApps.Metro.Controls.Dialogs
 Imports System.Collections.Concurrent
-Imports System.Runtime.InteropServices
 Imports System.Management
-Imports System.Net.Http
+Imports System.Net
+
+
+
 
 Public Class MainWindow
     Inherits MetroWindow
 
     Public Shared serverlist As List(Of DataServerList) = New List(Of DataServerList)
-
     Dim inputFile As String = "\\gsodata2\PSS Data\~Programs\~PSC Scripts\Server Storage Stats\input files\target servers.txt"
     Dim myservers As DataServerList = New DataServerList()
     Dim selectedList As DataServerList = New DataServerList()
@@ -27,81 +26,25 @@ Public Class MainWindow
 
     Private Sub Window_Loaded(ByVal sender As System.Object, ByVal e As System.EventArgs)
         serverlist.Clear()
+        ListShares()
         ReadInputFile()
         TB_ServerList.Text = "\\gsodata2\PSS Data\~Programs\~PSC Scripts\Server Storage Stats\input files\target servers.txt"
         rb_seperate.IsChecked = True
     End Sub
 
 
-
-
-    'Structure DataServerList
-    '    Dim ServerId As String
-    '    Dim ServerName As String
-    'End Structure
-
     Private Sub BT_Go_Click(sender As Object, e As RoutedEventArgs) Handles BT_Go.Click
 
         Dim search1 As String
         search1 = inputText.Text
-        'MsgBox(path)
-        '   Dim path As String = IO.Path.GetFullPath(IO.Path.Combine("\\ladata2", "\\CMA\\"))
-        Dim path1 As String = "\\ladata2\CMArchive01\"
-        Dim path2 As String = "\\ladata2\CMArchive02\"
-        Dim path3 As String = "\\ladata2\CMArchive03\"
-        Dim path4 As String = "\\ladata2\CMArchive04\"
-        Dim path5 As String = "\\ladata2\CMData01\"
-        Dim path6 As String = "\\ladata2\CMData02\"
-        Dim path7 As String = "\\ladata2\CMData03\"
-        Dim path8 As String = "\\ladata2\CMData04\"
-        Dim path9 As String = "\\ladata2\CMData05\"
-        Dim path11 As String = "\\ladata2\CMImages01\"
-        Dim path12 As String = "\\ladata2\CMImages02\"
-        Dim path13 As String = "\\ladata2\CMImages03\"
-        Dim path14 As String = "\\ladata2\CMImages04\"
-        Dim path15 As String = "\\ladata2\CMImages05\"
-
-
-
-        Dim path1a As String = "\\bndata2\CMArchive01\"
-        Dim path5a As String = "\\bndata2\CMData01\"
-        Dim path6a As String = "\\bndata2\CMData02\"
-        Dim path11a As String = "\\bndata2\CMImages01\"
-        Dim path12a As String = "\\bndata2\CMImages02\"
-
-
-        'Dim dirs = path1
         Dim path As String = ""
         'Dim dirs As String = IO.Path.GetFullPath(IO.Path.Combine(path1, path2))
         Dim dirs As List(Of String) = New List(Of String)
-        dirs.Add(path1)
-        dirs.Add(path2)
-        dirs.Add(path3)
-        dirs.Add(path4)
-        dirs.Add(path5)
-        dirs.Add(path6)
-        dirs.Add(path7)
-        dirs.Add(path8)
-        dirs.Add(path9)
-        dirs.Add(path11)
-        dirs.Add(path12)
-        dirs.Add(path13)
-        dirs.Add(path14)
-        dirs.Add(path15)
-
-        dirs.Add(path1a)
-        dirs.Add(path5a)
-        dirs.Add(path6a)
-        dirs.Add(path11a)
-        dirs.Add(path12a)
-
-
-
 
         If (rb_seperate.IsChecked) Then
+
             If (search1.Contains("-") Or search1.Contains("_")) Then
                 MsgBox("Search by matter instead")
-
             Else
                 Dim searchPattern As String = "*" & inputText.Text & "*"
                 Dim lines() As String = Split(searchPattern, vbCrLf)
@@ -110,7 +53,6 @@ Public Class MainWindow
                     For Each d In dirs
                         CollectFiles(d, li)
                     Next
-
                 Next
 
                 FirstGrid.Visibility = Visibility.Hidden
@@ -126,19 +68,95 @@ Public Class MainWindow
                 For Each d In dirs
                     CollectFiles(d, li)
                 Next
-
             Next
-
             FirstGrid.Visibility = Visibility.Hidden
             SecondGrid.Visibility = Visibility.Hidden
             ThirdGrid.Visibility = Visibility.Visible
         End If
-
     End Sub
 
 
+    Public Function ReadInputFile() As Integer
+
+        Dim linecount As Integer = 0
+        Using sr As New StreamReader(inputFile)
+
+            While Not sr.EndOfStream
+                Dim line As String = sr.ReadLine
+                Dim skipLine = sr.ReadLine
+                Dim columns As String() = skipLine.Split(",")
+                myservers.serverName = columns(1)
+                serverlist.Add(New DataServerList() With {
+                            .serverid = columns(0),
+                            .serverName = columns(1),
+                            .IsChecked = True
+                            })
+            End While
+            listView1.ItemsSource = serverlist
+        End Using
+        Return 1
+    End Function
+
+    Public Sub ListShares()
+        Dim Path As ManagementPath = New ManagementPath
+        Dim Shares As ManagementClass = Nothing
+        Dim CO As ConnectionOptions = New ConnectionOptions
+        CO.Impersonation = ImpersonationLevel.Impersonate
+        CO.Authentication = AuthenticationLevel.Default
+        CO.EnablePrivileges = True
+        CO.Username = "cmsvc"
+        CO.Password = "cm2016lw!"
+        Path.Server = "LVDDATA2" ' use . for local server, else server name
+
+        Path.NamespacePath = "\root\cimv2"
+        Path.RelativePath = "Win32_Share"
+
+        Dim password As String = "password"
+        Dim username As String = "username"
+        Dim domain As String = "domain"
+
+        Dim theNetworkCredential As NetworkCredential = New NetworkCredential(username, password, domain)
+        Dim theNetcache As CredentialCache = New CredentialCache()
+        CredentialCache.DefaultNetworkCredentials.Domain = domain
+        CredentialCache.DefaultNetworkCredentials.UserName = username
+        CredentialCache.DefaultNetworkCredentials.Password = password
 
 
+        Dim lNetWorkCredential As List(Of System.Net.NetworkCredential) = New System.Collections.Generic.List(Of System.Net.NetworkCredential)
+        lNetWorkCredential.Add(theNetworkCredential)
+
+        Dim Scope As ManagementScope = New ManagementScope(Path, CO)
+        Scope.Connect()
+
+        'If (Scope.IsConnected) Then
+        '    MsgBox("Connected")
+        'Else
+        '    MsgBox("Error Connecting")
+        'End If
+
+
+        Dim os As ManagementClass = New ManagementClass("Win32_OperatingSystem")
+        Dim inParams As ManagementBaseObject = os.GetMethodParameters("Win32Shutdown")
+        inParams("Flags") = "2"
+        inParams("Reserved") = "0"
+
+        Dim Options As ObjectGetOptions = New ObjectGetOptions(Nothing, New TimeSpan(0, 0, 0, 5), True)
+        Try
+            Shares = New ManagementClass(Scope, Path, Options)
+            Dim MOC As ManagementObjectCollection = Shares.GetInstances()
+            For Each mo As ManagementObject In MOC
+                Console.WriteLine("{0} - {1} - {2}", mo("Name"), mo("Description"), mo("Path"))
+            Next
+
+        Catch ex As Exception
+            Console.WriteLine(ex.Message)
+        Finally
+
+            If Shares IsNot Nothing Then
+                Shares.Dispose()
+            End If
+        End Try
+    End Sub
 
     Public Function CollectFiles(ByVal directory As String, ByVal pattern As String) As List(Of String)
         Dim queue As ConcurrentQueue(Of String) = New ConcurrentQueue(Of String)()
@@ -149,42 +167,23 @@ Public Class MainWindow
     Private Sub InternalCollectFiles(ByVal dir As String, ByVal pattern As String, ByVal queue As ConcurrentQueue(Of String))
         Dim directory As DirectoryInfo = New DirectoryInfo(dir)
         Dim dire = directory.GetDirectories(pattern)
+        Dim sb As New StringBuilder()
+        Dim output = "\\lvdiprodata\EXPORTS01\PS100000\Erin Development Projects\Output\output.txt"
 
         For Each result In dire.[Select](Function(file) file.FullName)
             queue.Enqueue(result)
             resultsBox.Items.Add(result)
+            For Each item As Object In resultsBox.Items
+                sb.AppendFormat("{0} {1}", item, Environment.NewLine)
+            Next
+            Dim writer As New System.IO.StreamWriter(output)
+            writer.Write(sb.ToString())
+            writer.WriteLine()
+            writer.Close()
         Next
-
+        MsgBox("Written to file")
     End Sub
 
-    Public Function GetDir(ByVal path As String, ByVal pattern As String)
-
-        Dim sb As New StringBuilder()
-        '    Dim output = "\\lvdiprodata\EXPORTS01\PS100000\Erin Development Projects\Output\output.txt"
-
-        Try
-            Dim directory As DirectoryInfo = New DirectoryInfo(path)
-            Dim dir As DirectoryInfo() = directory.GetDirectories(pattern, SearchOption.TopDirectoryOnly)
-            For Each di In dir
-                MsgBox(di)
-                ' resultsBox.Items.Add(di)
-                ' For Each item As Object In resultsBox.Items
-                'sb.AppendFormat("{0} {1}", item, Environment.NewLine)
-                'Next
-                '   Dim writer As New System.IO.StreamWriter(output)
-                '   writer.Write(sb.ToString())
-                '   writer.WriteLine()
-                '   writer.Close()
-            Next
-
-            ' MsgBox("Text successfully written to file")
-
-        Catch e As Exception
-            MsgBox("No access")
-            Exit Try
-        End Try
-        Return 1
-    End Function
 
     Private Sub BT_Input_Click(sender As Object, e As RoutedEventArgs) Handles BT_Input.Click
         Dim dlg As New Microsoft.Win32.OpenFileDialog
@@ -224,29 +223,6 @@ Public Class MainWindow
 
     End Sub
 
-    Public Function ReadInputFile() As Integer
-
-        Dim linecount As Integer = 0
-        Using sr As New StreamReader(inputFile)
-
-            While Not sr.EndOfStream
-                Dim line As String = sr.ReadLine
-                Dim skipLine = sr.ReadLine
-                Dim columns As String() = skipLine.Split(",")
-                myservers.serverName = columns(1)
-                serverlist.Add(New DataServerList() With {
-                            .serverid = columns(0),
-                            .serverName = columns(1),
-                            .IsChecked = True
-                            })
-                '.IsChecked = True
-                '})
-
-            End While
-            listView1.ItemsSource = serverlist
-        End Using
-        Return 1
-    End Function
 
     Private Sub chkSelectAll_Checked(ByVal sender As Object, e As RoutedEventArgs)
         'For i As Integer = 0 To listView1.Items.Count - 1
